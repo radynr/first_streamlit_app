2e677c1a5cef035b9b7fa6e0f9950eb4034fdf53
radynr <63967746+radynr@users.noreply.github.com>
2023-03-19 23:21:23 +0800

    Update streamlit_app.py
    fixed insert error

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-my_cur.execute('insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('+ add_my_fruit +')')
+my_cur.execute("insert into fruit_load_list values ('"+ add_my_fruit +"')")
 
 streamlit.write('Thanks to add ', add_my_fruit)
